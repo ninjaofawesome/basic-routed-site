f716fc005a1305843a5af25ef69d6175b6d6d6ca
Hannah Nordgren <hey@hereshannahs.info>
2021-07-06 23:51:18 -0400

    [WIP] routes now showing up

--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
-import { Router } from 'react-router-dom';
-import routes from './utils/routes';
+import createRoutes from './utils/routes';
 
-const trackingId = process.env.REACT_APP_TRACKING_ID; 
-ReactGA.initialize(trackingId);
-ReactGA.set({
-  // any data that is relevant to the user session
-  // that you would like to track with google analytics
-})
+const routes = createRoutes();
 
-const history = createBrowserHistory();
-
-// Initialize google analytics page view tracking
-history.listen(location => {
-  ReactGA.set({ page: location.pathname }); // Update the user's current page
-  ReactGA.pageview(location.pathname); // Record a pageview for the given page
-});
-
-const AppComponent = () => (
-  <Router history={history} routes={routes} />
-);
-
-ReactDOM.render(<AppComponent />, document.getElementById('root'));
+ReactDOM.render(routes, document.getElementById('root'));
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import { Route } from 'react-router';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+import { Router, Route } from 'react-router';
 import { GlobalStyle } from './globalStyles';
 import App from '../pages/App';
 import About from '../pages/About';
 
-const routes = (
-  <>
-  <GlobalStyle />
-  <Route path="/" component={App}>
-    <Route path="about" component={About} />
-  </Route>
-  </>
+const trackingId = process.env.REACT_APP_TRACKING_ID; 
+ReactGA.initialize(trackingId);
+ReactGA.set({
+  // any data that is relevant to the user session
+  // that you would like to track with google analytics
+});
+
+
+const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
+const createRoutes = () => (
+  <Router history={history}>
+    <GlobalStyle />
+    <Route path="/">
+      <App />
+    </Route>
+    <Route path="about">
+      <About />
+    </Route>
+  </Router>
 );
 
-export default routes;
+export default createRoutes;
