b805ea37d7cbaeae98c4b1ad8f2403d082c7f9ab
Hannah Nordgren <hey@hereshannahs.info>
2021-07-09 00:05:43 -0400

    [FEATURE] refactor routes to separate file

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,41 +1,22 @@
 import React from 'react';
-import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
-import { Router, Route, Switch } from 'react-router';
-import { GlobalStyle } from './globalStyles';
-import App from '../pages/App';
-import About from '../pages/About';
-import Header from '../components/Header';
+import {
+  Switch,
+  Route
+} from "react-router-dom";
+import AppPage from '../pages/App';
+import AboutPage from '../pages/About';
 
-const trackingId = process.env.REACT_APP_TRACKING_ID; 
-ReactGA.initialize(trackingId);
-ReactGA.set({
-  // any data that is relevant to the user session
-  // that you would like to track with google analytics
-});
-
-
-const history = createBrowserHistory();
-
-// Initialize google analytics page view tracking
-history.listen(location => {
-  ReactGA.set({ page: location.pathname }); // Update the user's current page
-  ReactGA.pageview(location.pathname); // Record a pageview for the given page
-});
-
-const createRoutes = () => (
-  <Router history={history}>
-    <GlobalStyle />
-      <Header />
-      <Switch>
-        <Route path='/about'>
-          <About />
-        </Route>
-        <Route path='/'>
-          <App />
-        </Route>
-      </Switch>
-  </Router>
+const RouteComponent = () => (
+  <div>
+    <Switch>
+      <Route path='/about'>
+        <AboutPage />
+      </Route>
+      <Route path='/'>
+        <AppPage />
+      </Route>
+    </Switch>
+  </div>
 );
 
-export default createRoutes;
+export default RouteComponent; 
